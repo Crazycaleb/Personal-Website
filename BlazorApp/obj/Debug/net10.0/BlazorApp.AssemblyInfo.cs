6ba294b6ed228b9bf2e84e7e738eb0a214382578
Crazycaleb <cjcarlton03@gmail.com>
2026-08-17 16:12:13 -0400

    First Run & Debug

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecc7a4fdb636859dc00c91c177a58075c5e37d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
